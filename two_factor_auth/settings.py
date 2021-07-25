1ea94fead6357badc7757e0500eb132bcad44fdb
MagicRom06 <romainhenry60@gmail.com>
2021-07-25 16:51:41 +0200

    update docker compose and settings for deploy

--- two_factor_auth/settings.py
+++ two_factor_auth/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v$j9vq()um.'
-'(9(s@livndq!9l+&)7@8k8o87bm9fp2(^-6g8$ms'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
