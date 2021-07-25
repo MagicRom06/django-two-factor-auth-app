bd0f86d1ffc44823619e1af326ea956e79b48286
MagicRom06 <romainhenry60@gmail.com>
2021-07-25 17:17:15 +0200

    fix settings for heroku

--- two_factor_auth/settings.py
+++ two_factor_auth/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'two-factorauth-app.herokuapp.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
